09b4aec8477ac39430ddc49c0f7e6f9fde8ebe1d
dannygardner26 <dannygardenhose@gmail.com>
2024-12-09 17:04:54 -0500

    modified:   CreatePosting.html 	new file:   account.java 	modified:   index.html 	new file:   posting.java

---
+++ posting.java
@@ -0,0 +1,16 @@
+public class posting {
+    private String companyName;
+    private String jobTitle;
+
+    private String jobDescription;
+
+    private String skills;
+    private int startingSalary;
+    private String location;
+    private 
+
+    public posting(String companyName, String jobTitle, String jobDescription, String skills, int startingSalary, String location){
+        
+    }
+
+}
